0cebe92eb9b35561f2ac62a8ab54cf8697a8b500
handwoong <handwoong@gmail.com>
2023-04-16 13:19:26 +0900

    :sparkles: 매장 엔티티 추가

---
+++ src/main/java/com/handwoong/everyonewaiter/domain/Store.java
@@ -0,0 +1,50 @@
+package com.handwoong.everyonewaiter.domain;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.handwoong.everyonewaiter.dto.store.StoreRequestDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Long id;
+
+    @NotNull
+    @Column(length = 50)
+    private String name;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder(access = PRIVATE)
+    private Store(String name, Member member) {
+        this.name = name;
+        this.member = member;
+    }
+
+    public static Store createStore(StoreRequestDto storeDto, Member member) {
+        return Store.builder()
+                .name(storeDto.getName())
+                .member(member)
+                .build();
+    }
+}
