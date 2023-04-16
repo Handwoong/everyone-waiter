c12b53d6bdc9d30fca657c1146c580b5b356d124
handwoong <handwoong@gmail.com>
2023-04-16 17:29:04 +0900

    :sparkles: 회원 레포지토리 이메일 조회 추가

--- src/main/java/com/handwoong/everyonewaiter/repository/MemberRepository.java
+++ src/main/java/com/handwoong/everyonewaiter/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.handwoong.everyonewaiter.repository;
 
 import com.handwoong.everyonewaiter.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -8,4 +9,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
+
+    Optional<Member> findByEmail(String email);
 }
