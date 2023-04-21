8c1d227bc40508f30e30d28fa45fb29fbe4419cc
handwoong <handwoong@gmail.com>
2023-04-22 03:20:22 +0900

    :sparkles: 시큐리티 UserDetailsService 구현

---
+++ src/main/java/com/handwoong/everyonewaiter/config/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.handwoong.everyonewaiter.config.security;
+
+import com.handwoong.everyonewaiter.domain.Member;
+import com.handwoong.everyonewaiter.repository.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원 입니다."));
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(member.getRole().name()));
+        return new MemberContext(member, roles);
+    }
+}
