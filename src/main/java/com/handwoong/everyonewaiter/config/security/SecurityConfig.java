6a44430ac97db09e9ad966c132ff4eb774f38901
handwoong <handwoong@gmail.com>
2023-04-22 21:03:56 +0900

    :sparkles: 시큐리티 로그인 성공 핸들러 적용

--- src/main/java/com/handwoong/everyonewaiter/config/security/SecurityConfig.java
+++ src/main/java/com/handwoong/everyonewaiter/config/security/SecurityConfig.java
@@ -11,12 +11,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
     @Bean
     public AuthenticationManager authenticationManager(
@@ -37,17 +40,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/", "/members/register", "/assets/**", "/demo.files/**")
-                .permitAll()
-                .requestMatchers("/mypage").hasRole("USER")
-                .requestMatchers("/messages").hasRole("MANAGER")
-                .requestMatchers("/config").hasRole("ADMIN")
+                .requestMatchers("/", "/members/register").permitAll()
+                .requestMatchers("/assets/**", "/demo.files/**").permitAll()
+                .requestMatchers("/members/**", "/stores/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated());
 
         http.formLogin()
                 .loginPage("/members/login")
                 .loginProcessingUrl("/members/login")
                 .defaultSuccessUrl("/")
+                .successHandler(authenticationSuccessHandler)
                 .permitAll();
 
         return http.build();
