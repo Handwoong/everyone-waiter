5fd06e3c6eb290806674876b5e1b8dcd3a01b61a
handwoong <handwoong@gmail.com>
2023-04-23 21:25:46 +0900

    :sparkles: 회원 서비스 공통 로직 메소드로 분리

--- src/main/java/com/handwoong/everyonewaiter/service/MemberServiceImpl.java
+++ src/main/java/com/handwoong/everyonewaiter/service/MemberServiceImpl.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 @Service
@@ -42,33 +43,11 @@ public class MemberServiceImpl implements MemberService {
         return member.getId();
     }
 
-    private void isExistsPhoneNumber(MemberDto memberDto) {
-        boolean isExistsPhoneNumber = memberRepository.existsByPhoneNumber(
-                memberDto.getPhoneNumber());
-        if (isExistsPhoneNumber) {
-            log.error("휴대폰 번호 중복 가입 요청 = 로그인 아이디 : '{}', 휴대폰 번호 : '{}'",
-                    memberDto.getUsername(), memberDto.getPhoneNumber());
-            throw new ResourceExistsException("이미 존재하는 휴대폰 번호 입니다.");
-        }
-    }
-
-    private void isExistsUsername(MemberDto memberDto) {
-        boolean isExistsUsername = memberRepository.existsByUsername(
-                memberDto.getUsername());
-        if (isExistsUsername) {
-            log.error("로그인 아이디 중복 가입 요청 = 로그인 아이디 : '{}'", memberDto.getUsername());
-            throw new ResourceExistsException("이미 존재하는 아이디 입니다.");
-        }
-    }
-
     @Override
     public MemberResponseDto findMember(Long memberId) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    log.error("존재하지 않는 회원 찾기 = 찾으려는 아이디 : '{}'", memberId);
-                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
-                });
+        Member member = findById(memberId);
         MemberResponseDto memberDto = MemberResponseDto.from(member);
+
         log.info("회원 조회 = 아이디 : '{}', 로그인 아이디 : '{}', 잔액 : '{}'",
                 memberDto.getId(), memberDto.getUsername(), memberDto.getBalance());
         return memberDto;
@@ -76,20 +55,19 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public MemberResponseDto findMemberByUsername(String username) {
-        Member member = memberRepository.findByUsername(username)
-                .orElseThrow(() -> {
-                    log.error("존재하지 않는 회원 찾기 = 찾으려는 로그인 아이디 : '{}'", username);
-                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
-                });
+        Member member = findByUsername(username);
         MemberResponseDto memberDto = MemberResponseDto.from(member);
+
         log.info("회원 조회 = 아이디 : '{}', 로그인 아이디 : '{}', 잔액 : '{}'",
                 memberDto.getId(), memberDto.getUsername(), memberDto.getBalance());
         return memberDto;
     }
 
+
     @Override
     public List<MemberResponseDto> findMemberList() {
         List<Member> members = memberRepository.findAll();
+
         log.info("회원 전체 목록 조회 = '{}'명", members.size());
         return members.stream()
                 .map(MemberResponseDto::from)
@@ -100,19 +78,11 @@ public class MemberServiceImpl implements MemberService {
     @Transactional
     public BasicResponseDto changePassword(String username,
             MemberPasswordDto passwordDto) {
-        Member member = memberRepository.findByUsername(username)
-                .orElseThrow(() -> {
-                    log.error("존재하지 않는 회원 비밀번호 변경 요청 = 찾으려는 로그인 아이디 : '{}'", username);
-                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
-                });
-
-        String password = passwordDto.getCurrentPassword();
-        if (!passwordEncoder.matches(password, member.getPassword())) {
-            log.error("비밀번호 변경 요청 기존 비밀번호가 일치하지 않음 = 로그인 아이디 : '{}'", username);
-            throw new ResourceNotMatchException("기존 비밀번호가 일치하지 않습니다.");
-        }
-
+        Member member = findByUsername(username);
+        matchPassword(member, passwordDto.getCurrentPassword());
         member.encodePassword(passwordEncoder.encode(passwordDto.getNewPassword()));
+
+        log.info("비밀번호 변경 성공 = 로그인 아이디 : '{}'", username);
         return new BasicResponseDto("success");
     }
 
@@ -120,20 +90,61 @@ public class MemberServiceImpl implements MemberService {
     @Transactional
     public BasicResponseDto deleteMember(String username,
             MemberPasswordDto passwordDto) {
-        Member member = memberRepository.findByUsername(username)
-                .orElseThrow(() -> {
-                    log.error("존재하지 않는 회원 탈퇴 요청 = 찾으려는 로그인 아이디 : '{}'", username);
-                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
-                });
-
-        String password = passwordDto.getCurrentPassword();
-        if (!passwordEncoder.matches(password, member.getPassword())) {
-            log.error("회원 탈퇴 요청 비밀번호가 일치하지 않음 = 로그인 아이디 : '{}'", username);
-            throw new ResourceNotMatchException("비밀번호가 일치하지 않습니다.");
-        }
-
+        Member member = findByUsername(username);
+        matchPassword(member, passwordDto.getCurrentPassword());
         memberRepository.deleteById(member.getId());
+
         log.info("회원 삭제 성공 = 로그인 아이디 : '{}'", username);
         return new BasicResponseDto("success");
+    }
+
+    private void isExistsPhoneNumber(MemberDto memberDto) {
+        boolean isExistsPhoneNumber = memberRepository.existsByPhoneNumber(
+                memberDto.getPhoneNumber());
+
+        if (isExistsPhoneNumber) {
+            log.error("휴대폰 번호 중복 가입 요청 = 로그인 아이디 : '{}', 휴대폰 번호 : '{}'",
+                    memberDto.getUsername(), memberDto.getPhoneNumber());
+            throw new ResourceExistsException("이미 존재하는 휴대폰 번호 입니다.");
+        }
+    }
+
+    private void isExistsUsername(MemberDto memberDto) {
+        boolean isExistsUsername = memberRepository.existsByUsername(
+                memberDto.getUsername());
+
+        if (isExistsUsername) {
+            log.error("로그인 아이디 중복 가입 요청 = 로그인 아이디 : '{}'", memberDto.getUsername());
+            throw new ResourceExistsException("이미 존재하는 아이디 입니다.");
+        }
+    }
+
+    private Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> {
+                    log.error("[{}] 존재하지 않는 회원 찾기 = 찾으려는 아이디 : '{}'",
+                            TransactionSynchronizationManager.getCurrentTransactionName(),
+                            memberId);
+                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
+                });
+    }
+
+    private Member findByUsername(String username) {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    log.error("[{}] 존재하지 않는 회원 찾기 = 찾으려는 로그인 아이디 : '{}'",
+                            TransactionSynchronizationManager.getCurrentTransactionName(),
+                            username);
+                    return new ResourceNotFoundException("존재하지 않는 회원입니다.");
+                });
+    }
+
+    private void matchPassword(Member member, String password) {
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            log.error("[{}] 기존 비밀번호가 일치하지 않음 = 로그인 아이디 : '{}'",
+                    TransactionSynchronizationManager.getCurrentTransactionName(),
+                    member.getUsername());
+            throw new ResourceNotMatchException("기존 비밀번호가 일치하지 않습니다.");
+        }
     }
 }
