ebb7ad138cb0efd7266066a107fea6f31b6f1c69
handwoong <handwoong@gmail.com>
2023-04-24 02:29:19 +0900

    :white_check_mark: 웨이팅 레포지토리 조회 페이징 테스트 추가

--- src/test/java/com/handwoong/everyonewaiter/repository/WaitingRepositoryTest.java
+++ src/test/java/com/handwoong/everyonewaiter/repository/WaitingRepositoryTest.java
@@ -1,21 +1,22 @@
 package com.handwoong.everyonewaiter.repository;
 
+import static com.handwoong.everyonewaiter.domain.WaitingStatus.DEFAULT;
+import static com.handwoong.everyonewaiter.domain.WaitingStatus.ENTER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.handwoong.everyonewaiter.domain.Member;
 import com.handwoong.everyonewaiter.domain.Store;
 import com.handwoong.everyonewaiter.domain.Waiting;
-import com.handwoong.everyonewaiter.domain.WaitingStatus;
 import com.handwoong.everyonewaiter.dto.member.MemberDto;
 import com.handwoong.everyonewaiter.dto.store.StoreDto;
 import com.handwoong.everyonewaiter.dto.waiting.WaitingDto;
-import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
 
 @DataJpaTest
 class WaitingRepositoryTest {
@@ -44,7 +45,7 @@ class WaitingRepositoryTest {
 
         WaitingDto waitingDto = new WaitingDto(4, 2, "01012345678");
         waiting = waitingRepository.save(
-                Waiting.createWaiting(waitingDto, store, null, new ArrayList<>()));
+                Waiting.createWaiting(waitingDto, store, null, null));
     }
 
     @Test
@@ -62,14 +63,32 @@ class WaitingRepositoryTest {
     void findWaitingList() throws Exception {
         // when
         List<Waiting> waitingList = waitingRepository
-                .findDefaultWaitingList(store.getId());
+                .findStatusWaitingList(store.getId(), DEFAULT);
         Waiting waitingA = waitingList.get(0);
 
         // then
         assertThat(waitingList.size()).isEqualTo(1);
         assertThat(waitingA.getWaitingNumber()).isEqualTo(1);
         assertThat(waitingA.getWaitingTurn()).isEqualTo(0);
-        assertThat(waitingA.getStatus()).isEqualTo(WaitingStatus.DEFAULT);
+        assertThat(waitingA.getStatus()).isEqualTo(DEFAULT);
+    }
+
+    @Test
+    @DisplayName("웨이팅 상태로 마지막 웨이팅 조회")
+    void findStatusLastWaiting() throws Exception {
+        // given
+        waiting.changeEnterOrCancelStatus(ENTER);
+
+        // when
+        List<Waiting> findWaitingList = waitingRepository
+                .findStatusLastWaiting(store.getId(), ENTER, PageRequest.of(0, 1));
+        Waiting findWaiting = findWaitingList.get(0);
+
+        // then
+        assertThat(findWaitingList.size()).isEqualTo(1);
+        assertThat(findWaiting.getWaitingNumber()).isEqualTo(1);
+        assertThat(findWaiting.getWaitingTurn()).isEqualTo(-1);
+        assertThat(findWaiting.getStatus()).isEqualTo(ENTER);
     }
 
     @Test
@@ -78,17 +97,17 @@ class WaitingRepositoryTest {
         // given
         WaitingDto waitingDto = new WaitingDto(4, 2, "01012345678");
         Waiting newWaiting = waitingRepository.save(
-                Waiting.createWaiting(waitingDto, store, waiting,
-                        new ArrayList<>(List.of(waiting))));
+                Waiting.createWaiting(waitingDto, store, waiting, waiting));
 
         // when
-        Waiting lastWaiting = waitingRepository.findLastWaiting(store.getId());
+        Waiting lastWaiting = waitingRepository.findLastWaiting(store.getId(),
+                PageRequest.of(0, 1)).get(0);
 
         // then
         assertThat(lastWaiting.getId()).isEqualTo(newWaiting.getId());
         assertThat(lastWaiting.getWaitingNumber()).isEqualTo(2);
         assertThat(lastWaiting.getWaitingTurn()).isEqualTo(1);
-        assertThat(lastWaiting.getStatus()).isEqualTo(WaitingStatus.DEFAULT);
+        assertThat(lastWaiting.getStatus()).isEqualTo(DEFAULT);
     }
 
     @Test
@@ -96,8 +115,8 @@ class WaitingRepositoryTest {
     void greaterThanDecreaseTurn() throws Exception {
         // given
         WaitingDto waitingDto = new WaitingDto(4, 2, "01012345678");
-        waitingRepository.save(Waiting.createWaiting(waitingDto, store, waiting,
-                new ArrayList<>(List.of(waiting))));
+        waitingRepository.save(
+                Waiting.createWaiting(waitingDto, store, waiting, waiting));
 
         // when
         int changeCount = waitingRepository.decreaseWaitingTurn(store.getId(), 0);
