540be1d72327f2b0b8c724847105f936dab29a94
handwoong <handwoong@gmail.com>
2023-04-25 00:20:49 +0900

    :bug: 매장 DTO 빈 생성자 추가

--- src/main/java/com/handwoong/everyonewaiter/dto/store/StoreDto.java
+++ src/main/java/com/handwoong/everyonewaiter/dto/store/StoreDto.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class StoreDto {
 
     @NotNull(message = "{error.message.null}")
