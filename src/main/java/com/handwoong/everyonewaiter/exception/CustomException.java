6d097abd26f9130d92c89b0a2744da9d03b3e52e
handwoong <handwoong@gmail.com>
2023-04-27 01:25:13 +0900

    :sparkles: 예외 클래스 하나로 통합

---
+++ src/main/java/com/handwoong/everyonewaiter/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.handwoong.everyonewaiter.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
