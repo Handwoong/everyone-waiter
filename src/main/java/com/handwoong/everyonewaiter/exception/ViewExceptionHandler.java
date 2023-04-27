f4ebd6d5e2fc0e91930ec4a1654eb75548f69abc
handwoong <handwoong@gmail.com>
2023-04-27 22:46:17 +0900

    :recycle: View 예외 핸들러 메소드 반환 타입 수정

--- src/main/java/com/handwoong/everyonewaiter/exception/ViewExceptionHandler.java
+++ src/main/java/com/handwoong/everyonewaiter/exception/ViewExceptionHandler.java
@@ -2,6 +2,7 @@ package com.handwoong.everyonewaiter.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,17 +14,19 @@ import org.springframework.web.servlet.view.RedirectView;
 public class ViewExceptionHandler {
 
     @ExceptionHandler({CustomException.class})
-    public Object handleCustomException(CustomException err, HttpServletRequest req,
+    public RedirectView handleCustomException(CustomException err, HttpServletRequest req,
             RedirectAttributes redirectAttr) {
         ErrorCode errorCode = err.getErrorCode();
         log.error("[{}] {} {}", errorCode.name(), errorCode.getStatus(), errorCode.getMessage());
 
+        String viewName = getRedirectUri(req, errorCode);
         redirectAttr.addFlashAttribute("errorCode", errorCode);
-        return new RedirectView(req.getRequestURI());
+
+        return new RedirectView(viewName);
     }
 
     @ExceptionHandler({Exception.class})
-    public Object handleException(Exception err) {
+    public ModelAndView handleException(Exception err) {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         log.error("[{}] {} {} {}",
                 errorCode.name(), errorCode.getStatus(), errorCode.getMessage(), err.getMessage());
@@ -34,12 +37,25 @@ public class ViewExceptionHandler {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("errorCode", errorCode);
 
-        switch (errorCode.getStatus()) {
-            case BAD_REQUEST -> modelAndView.setViewName("error/400");
-            case NOT_FOUND -> modelAndView.setViewName("error/404");
-            default -> modelAndView.setViewName("error/5xx");
-        }
+        String viewName = getViewName(errorCode);
+        modelAndView.setViewName(viewName);
 
         return modelAndView;
+    }
+
+    private String getViewName(ErrorCode errorCode) {
+        StringBuilder sb = new StringBuilder();
+
+        switch (errorCode.getStatus()) {
+            case BAD_REQUEST -> sb.append("error/400");
+            case NOT_FOUND -> sb.append("error/404");
+            default -> sb.append("error/5xx");
+        }
+
+        return sb.toString();
+    }
+
+    private String getRedirectUri(HttpServletRequest req, ErrorCode errorCode) {
+        return errorCode.getStatus() == HttpStatus.NOT_FOUND ? "/error" : req.getRequestURI();
     }
 }
