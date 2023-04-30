7bf5bdc8fe0369a072ed8f26ee23184a9b3bbaf1
handwoong <handwoong@gmail.com>
2023-05-01 05:35:47 +0900

    :sparkles: 매장 서비스 업데이트 및 삭제 인터페이스 정의

--- src/main/java/com/handwoong/everyonewaiter/service/StoreService.java
+++ src/main/java/com/handwoong/everyonewaiter/service/StoreService.java
@@ -8,6 +8,10 @@ public interface StoreService {
 
     Long register(String username, StoreReqDto storeDto);
 
+    void update(String username, Long storeId, StoreReqDto storeDto);
+
+    void delete(String username, Long storeId);
+
     StoreResDto findStore(String username, Long storeId);
 
     List<StoreResDto> findStoreList(String username);
