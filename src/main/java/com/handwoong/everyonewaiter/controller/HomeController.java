8f7a0b6c03f41d70980beeb0eaafca976c73f3b7
handwoong <handwoong@gmail.com>
2023-04-30 21:28:55 +0900

    :sparkles: 404 페이지 컨트롤러 추가

--- src/main/java/com/handwoong/everyonewaiter/controller/HomeController.java
+++ src/main/java/com/handwoong/everyonewaiter/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/error")
+    public String error() {
+        return "error/404";
+    }
 }
