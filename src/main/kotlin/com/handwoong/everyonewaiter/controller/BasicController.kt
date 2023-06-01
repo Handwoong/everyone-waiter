98fbd38c45145cd168ce269022ae7f056a0d70d9
handwoong <handwoong@gmail.com>
2023-06-02 07:24:02 +0900

    :sparkles: 홈 컨트롤러 추가

---
+++ src/main/kotlin/com/handwoong/everyonewaiter/controller/BasicController.kt
@@ -0,0 +1,12 @@
+package com.handwoong.everyonewaiter.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class BasicController {
+
+    @GetMapping("/")
+    fun homePage() = "home"
+
+}
