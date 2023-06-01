95353db0841f5c518ba181c0d9eb09c305d79364
handwoong <handwoong@gmail.com>
2023-06-02 07:30:55 +0900

    :sparkles: 시큐리티 유틸 파일 추가

--- src/main/kotlin/com/handwoong/everyonewaiter/util/SecurityUtils.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/util/SecurityUtils.kt
@@ -1,17 +1,39 @@
 package com.handwoong.everyonewaiter.util
 
+import com.handwoong.everyonewaiter.exception.ErrorCode.UN_AUTHORIZED
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
+import javax.servlet.http.Cookie
 
-fun getAuthentication(): Authentication {
+const val ONE_DAY_HOUR = 24
+const val HOUR_SECONDS = 3600
+
+fun getAuthentication(): Authentication? {
     return SecurityContextHolder.getContext().authentication
 }
 
-fun getUsername(): String {
-    return SecurityContextHolder.getContext().authentication.name
+fun getAuthenticationUsername(): String {
+    val authentication = getAuthentication()
+    if (authentication != null && isAuthentication()) {
+        return authentication.name
+    }
+    throwFail(UN_AUTHORIZED)
 }
 
 fun isAuthentication(): Boolean {
     val authentication = getAuthentication()
-    return authentication.principal !is String
+    return if (authentication != null) {
+        authentication.principal !is String
+    } else {
+        false
+    }
+}
+
+fun createCookie(accessToken: String): Cookie {
+    val cookie = Cookie("token", accessToken)
+    cookie.path = "/"
+    cookie.maxAge = ONE_DAY_HOUR * HOUR_SECONDS
+    cookie.isHttpOnly = true
+//    cookie.secure = true
+    return cookie
 }
