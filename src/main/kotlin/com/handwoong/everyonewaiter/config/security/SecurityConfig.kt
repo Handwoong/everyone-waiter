b1202913a36c3309ab66595d537c046fcb2fba8a
handwoong <handwoong@gmail.com>
2023-06-02 07:24:55 +0900

    :sparkles: 시큐리티 JWT 토큰 필터 적용

--- src/main/kotlin/com/handwoong/everyonewaiter/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/config/security/SecurityConfig.kt
@@ -1,39 +1,40 @@
 package com.handwoong.everyonewaiter.config.security
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(
+    private val jwtTokenAuthenticationFilter: JwtTokenAuthenticationFilter,
+) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
+        // 인증 정책
+        http.formLogin().disable()
+            .httpBasic().disable()
+            .csrf().disable()
+            .addFilterBefore(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
         // 인가 정책
         http.authorizeRequests()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .antMatchers("/webfonts/**").permitAll()
+            .antMatchers("/", "/members/login", "/members/register", "/error").permitAll()
             .anyRequest().authenticated()
 
-        // 인증 정책
-        http.formLogin()
-//            .loginPage("/loginPage")
-            .defaultSuccessUrl("/")
-            .failureUrl("/login?fail=ok")
-            .usernameParameter("userId")
-            .passwordParameter("passwd")
-            .loginProcessingUrl("/login_proc")
-//            .successHandler { _, response, authentication ->
-//                println(authentication.name)
-//                response.sendRedirect("/")
-//            }
-//            .failureHandler { _, response, exception ->
-//                println(exception.message)
-//                response.sendRedirect("/login")
-//            }
-            .permitAll()
+        http.exceptionHandling()
+            .accessDeniedHandler(AccessDeniedHandlerCustom())
+            .authenticationEntryPoint(AuthenticationEntryPointCustom())
     }
 
     @Bean
