1c4db1f9acc3dc4bc3289e6e66f8230201206b64
handwoong <handwoong@gmail.com>
2023-06-04 18:53:59 +0900

    :sparkles: 회원 API 컨트롤러 요청 데이터 유효성 검사 추가

--- src/main/kotlin/com/handwoong/everyonewaiter/controller/member/MemberApiController.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/controller/member/MemberApiController.kt
@@ -5,12 +5,13 @@ import com.handwoong.everyonewaiter.dto.member.MemberLoginRequest
 import com.handwoong.everyonewaiter.dto.member.TokenResponse
 import com.handwoong.everyonewaiter.service.member.MemberService
 import com.handwoong.everyonewaiter.util.createCookie
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
+import javax.validation.Valid
 
 @RestController
 class MemberApiController(
@@ -18,19 +19,21 @@ class MemberApiController(
 ) {
 
     @PostMapping("/members/login")
-    fun loginHandler(
+    fun loginMember(
         response: HttpServletResponse,
-        @RequestBody memberDto: MemberLoginRequest,
+        @RequestBody @Valid memberDto: MemberLoginRequest,
     ): TokenResponse {
         val tokenResponse = memberService.login(memberDto)
         response.addCookie(createCookie(tokenResponse.accessToken))
         return tokenResponse
     }
 
-    @PostMapping("/members/register")
-    fun registerHandler(@RequestBody memberDto: MemberCreateRequest): ResponseEntity<Unit> {
+    @PostMapping("/members")
+    fun registerMember(
+        @RequestBody @Valid memberDto: MemberCreateRequest,
+    ): ResponseEntity<Unit> {
         memberService.register(memberDto)
-        return ResponseEntity<Unit>(HttpStatus.CREATED)
+        return ResponseEntity<Unit>(CREATED)
     }
 
 }
