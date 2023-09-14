0c132bb0fbe1e61ebf304479929fd61b8768f999
handwoong <handwoong@gmail.com>
2023-09-14 18:30:37 +0900

    :art: 메뉴 서비스 파라미터 수정

--- src/main/kotlin/com/handwoong/everyonewaiter/service/menu/MenuService.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/service/menu/MenuService.kt
@@ -5,12 +5,12 @@ import com.handwoong.everyonewaiter.dto.menu.MenuResponse
 
 interface MenuService {
 
-    fun findAllStoreMenu(username: String, storeId: Long): List<MenuResponse>
+    fun findAllStoreMenu(storeId: Long): List<MenuResponse>
 
     fun register(menuDto: MenuRequest, username: String, categoryId: Long)
 
-    fun update(menuDto: MenuRequest, username: String, menuId: Long)
+    fun update(menuDto: MenuRequest, menuId: Long)
 
-    fun delete(username: String, menuId: Long)
+    fun delete(menuId: Long)
 
 }
