58a754672b0c3b782cf90a93f40279029a12b72d
handwoong <handwoong@gmail.com>
2023-09-14 18:32:03 +0900

    :sparkles: 메뉴 DTO 옵션 필드 추가

--- src/main/kotlin/com/handwoong/everyonewaiter/dto/menu/MenuRequests.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/dto/menu/MenuRequests.kt
@@ -22,6 +22,12 @@ data class MenuRequest(
     val status: MenuStatus = MenuStatus.BASIC,
     val image: String? = null,
 
+    val isSelectSpicy: Boolean,
+
+    val isPrintKitchen: Boolean,
+
+    val customOption: MutableList<MenuCustomOptionRequest>,
+
     @field:NotNull(message = "{error.message.null}")
     @field:Min(value = 0, message = "{error.message.min}")
     @field:Max(value = 10, message = "{error.message.max}")
@@ -29,4 +35,9 @@ data class MenuRequest(
 
     @field:NotNull(message = "{error.message.null}")
     val sortingSequence: Int,
+)
+
+data class MenuCustomOptionRequest(
+    val name: String,
+    val price: Int,
 )
--- src/main/kotlin/com/handwoong/everyonewaiter/dto/menu/MenuResponses.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/dto/menu/MenuResponses.kt
@@ -1,6 +1,7 @@
 package com.handwoong.everyonewaiter.dto.menu
 
 import com.handwoong.everyonewaiter.domain.menu.Menu
+import com.handwoong.everyonewaiter.domain.menu.MenuCustomOption
 import com.handwoong.everyonewaiter.domain.menu.MenuStatus
 
 data class MenuResponse(
@@ -12,12 +13,17 @@ data class MenuResponse(
     val price: Int,
     val status: MenuStatus,
     val image: String?,
+    val isSelectSpicy: Boolean,
+    val isPrintKitchen: Boolean,
+    val customOption: List<MenuCustomOptionResponse>,
     val spicy: Int,
     val sortingSequence: Int,
 ) {
 
     companion object {
         fun of(menu: Menu): MenuResponse {
+            val customOptionResponses = menu.customOption.map(MenuCustomOptionResponse::of)
+
             return MenuResponse(
                 id = menu.id!!,
                 name = menu.name,
@@ -27,8 +33,31 @@ data class MenuResponse(
                 price = menu.price,
                 status = menu.status,
                 image = menu.image,
+                isSelectSpicy = menu.option.isSelectSpicy,
+                isPrintKitchen = menu.option.isPrintKitchen,
+                customOption = customOptionResponses,
                 spicy = menu.spicy,
                 sortingSequence = menu.sortingSequence,
+            )
+        }
+    }
+
+}
+
+data class MenuCustomOptionResponse(
+    val id: Long,
+    val name: String,
+    val price: Int,
+) {
+
+    companion object {
+        fun of(
+            menuCustomOption: MenuCustomOption,
+        ): MenuCustomOptionResponse {
+            return MenuCustomOptionResponse(
+                id = menuCustomOption.id!!,
+                name = menuCustomOption.name,
+                price = menuCustomOption.price,
             )
         }
     }
