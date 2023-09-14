bd21370c042d58b28b7cd26a6b304d21dc2551c7
handwoong <handwoong@gmail.com>
2023-09-14 18:27:40 +0900

    :sparkles: 주문 레포지토리 인터페이스 추가

---
+++ src/main/kotlin/com/handwoong/everyonewaiter/repository/order/OrderRepositoryCustom.kt
@@ -0,0 +1,23 @@
+package com.handwoong.everyonewaiter.repository.order
+
+import com.handwoong.everyonewaiter.domain.order.Order
+import com.handwoong.everyonewaiter.domain.order.OrderStatus
+
+interface OrderRepositoryCustom {
+
+    fun existsOrder(
+        storeId: Long,
+        tableNumber: Int,
+    ): Boolean
+
+    fun findAllStoreOrder(
+        storeId: Long,
+        orderStatus: OrderStatus?,
+    ): List<Order>
+
+    fun findStoreTableOrderList(
+        storeId: Long,
+        tableNumber: Int? = null,
+    ): List<Order>
+
+}
