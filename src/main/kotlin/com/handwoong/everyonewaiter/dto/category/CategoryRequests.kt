348051f141b3623a0439a74e3227eca99efb29cb
handwoong <handwoong@gmail.com>
2023-09-14 18:31:37 +0900

    :sparkles: 카테고리 DTO 아이콘 필드 추가

--- src/main/kotlin/com/handwoong/everyonewaiter/dto/category/CategoryRequests.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/dto/category/CategoryRequests.kt
@@ -7,4 +7,5 @@ data class CategoryRequest(
     @field:NotNull(message = "{error.message.null}")
     @field:Size(min = 2, max = 20, message = "{error.message.size}")
     val name: String,
+    val icon: String,
 )
