caab6cf18a4aa52ced5997e02b84af110405d326
handwoong <handwoong@gmail.com>
2023-09-25 20:26:32 +0900

    :sparkles: 영수증 ResponseDTO 추가

---
+++ src/main/kotlin/com/handwoong/everyonewaiter/dto/receipt/ReceiptResponses.kt
@@ -0,0 +1,45 @@
+package com.handwoong.everyonewaiter.dto.receipt
+
+import com.handwoong.everyonewaiter.domain.receipt.Receipt
+import com.handwoong.everyonewaiter.domain.receipt.ReceiptMenu
+
+data class ReceiptResponse(
+    val id: Long,
+    val tableNumber: Int,
+    val memo: String,
+    val receiptMenuList: List<ReceiptMenuResponse>,
+) {
+
+    companion object {
+        fun of(receipt: Receipt): ReceiptResponse {
+            val receiptMenuResponseList = receipt.menuList.map { printMenu -> ReceiptMenuResponse.of(printMenu) }
+            return ReceiptResponse(
+                id = receipt.id!!,
+                tableNumber = receipt.tableNumber,
+                memo = receipt.memo,
+                receiptMenuList = receiptMenuResponseList,
+            )
+        }
+    }
+
+}
+
+data class ReceiptMenuResponse(
+    val name: String,
+    val qty: Int,
+    val optionName: String,
+    val customOption: String,
+) {
+
+    companion object {
+        fun of(receiptMenu: ReceiptMenu): ReceiptMenuResponse {
+            return ReceiptMenuResponse(
+                name = receiptMenu.name,
+                qty = receiptMenu.qty,
+                optionName = receiptMenu.optionName,
+                customOption = receiptMenu.customOption,
+            )
+        }
+    }
+
+}
