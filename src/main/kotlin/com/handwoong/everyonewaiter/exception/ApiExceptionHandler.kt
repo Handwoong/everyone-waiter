d6e7e98cbda5b9e9bf979539a10a567d9935e4be
handwoong <handwoong@gmail.com>
2023-10-11 21:47:29 +0900

    :sparkles: 인증 예외 추가

--- src/main/kotlin/com/handwoong/everyonewaiter/exception/ApiExceptionHandler.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/exception/ApiExceptionHandler.kt
@@ -2,6 +2,8 @@ package com.handwoong.everyonewaiter.exception
 
 import com.handwoong.everyonewaiter.util.logger
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.InternalAuthenticationServiceException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -22,6 +24,20 @@ class ApiExceptionHandler {
     fun handleNotValidException(error: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
         val errorCode = ErrorCode.METHOD_ARGUMENT_NOT_VALID
         log.error("[${errorCode.name}] ${errorCode.status} ${errorCode.message} ${error.message}")
+        return ErrorResponse.of(errorCode)
+    }
+
+    @ExceptionHandler(value = [BadCredentialsException::class])
+    fun handleBadCredentialsException(error: BadCredentialsException): ResponseEntity<ErrorResponse> {
+        val errorCode = ErrorCode.CHECK_USERNAME_AND_PASSWORD
+        log.error("[${errorCode.name}] ${errorCode.status} ${errorCode.message}")
+        return ErrorResponse.of(errorCode)
+    }
+
+    @ExceptionHandler(value = [InternalAuthenticationServiceException::class])
+    fun handleInternalAuthenticationServiceException(error: InternalAuthenticationServiceException): ResponseEntity<ErrorResponse> {
+        val errorCode = ErrorCode.CHECK_USERNAME_AND_PASSWORD
+        log.error("[${errorCode.name}] ${errorCode.status} ${errorCode.message}")
         return ErrorResponse.of(errorCode)
     }
 
