4d95823fb7bc39527377dc4bd4ad958775ddff14
handwoong <handwoong@gmail.com>
2023-10-12 23:16:04 +0900

    :recycle: 웹소켓 API 스펙 변경

--- src/main/kotlin/com/handwoong/everyonewaiter/dto/socket/SocketMessageDto.kt
+++ src/main/kotlin/com/handwoong/everyonewaiter/dto/socket/SocketMessageDto.kt
@@ -1,20 +1,6 @@
 package com.handwoong.everyonewaiter.dto.socket
 
-import com.handwoong.everyonewaiter.dto.order.OrderMenuOption
-
 data class OrderSocketMessageDto(
-    val storeId: String = "",
-    val message: MutableList<OrderMessage> = mutableListOf(),
-)
-
-data class OrderMessage(
-    val id: Long,
-    val menuId: Long,
-    val name: String,
-    val image: String,
-    val price: Int,
-    val qty: Int,
-    val isPrintKitchen: Boolean,
-    val option: OrderMenuOption,
-    val customOption: String,
+    val storeId: String,
+    val message: String,
 )
